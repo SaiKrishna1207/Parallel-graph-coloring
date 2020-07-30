3987ea3f9268ba9ffc61c67e2e9b5c2339c06c5d
dhanwin247 <dhanwinrao18@gmail.com>
2020-07-30 12:49:37 +0530

    update SDL approach

--- Parallel/PGC-Smallest-Degree-Last.cu
+++ Parallel/PGC-Smallest-Degree-Last.cu
@@ -97,18 +97,20 @@ __global__ void assignWeightKernel(int n, int k, int weight, int* d_adj, int* d_
     }
 }
 
-__global__ void colorKernel(int n, int weight, int maxColors, int* d_adj, int* d_adj_p, int* d_weights, int* d_colors, bool* d_sameWt)
-{
-    int index = (blockDim.x * blockIdx.x) + threadIdx.x;    
-    if(index >= n || d_colors[index]!=-1 || d_weights[index]!=weight)
-        return;
-    
-    //tried atomically changing color and then checking for conflicts with neighbors with the same weight
-    //but it gave the same color for all the vertices lmao
-    //if we do it with that detect conflicts do-while loop from naive implementation, it will lead to no significant gain in time complexity
-    
-        
-
+void randomWeightAssign(int* h_rweights, int n){                                             //Assign random weights to the vertices
+    int i, j = 1;
+    vector<int> arr(n);
+    for(i = 0; i < n; i++)
+        arr[i] = i;
+    i = 0;
+    while(i < n){
+        int x = rand() % arr.size();
+        h_rweights[arr[x]] = j;
+        j++;
+        arr.erase(arr.begin() + x);
+        i++;
+    }
+    return;
 }
 
 void assignWeight(int n, int* d_adj, int* d_adj_p, int* h_weights, bool* h_visited)
@@ -145,6 +147,83 @@ void assignWeight(int n, int* d_adj, int* d_adj_p, int* h_weights, bool* h_visit
     cudaMemcpy(h_weights, d_weights, sizeof(int) * n, cudaMemcpyDeviceToHost);
 }
 
+__global__ void initializeRemCol(bool* d_rem, int* d_colours, int n){               //Set uncolored vertex set to true and initial colour to invalid(0)
+    int ind = (blockDim.x * blockIdx.x) + threadIdx.x;
+    if(ind >= n)
+        return;
+    d_rem[ind] = true;
+    d_colours[ind] = 0;
+} 
+
+__global__ void colorSet(int* d_adj, int* d_adj_p, int* d_weights, bool* d_rem, int* d_colours, int n, int maxDegree){//pass h_deg here
+    int index = (blockDim.x * blockIdx.x) + threadIdx.x;    
+    if(index >= n)
+        return;
+    
+    if(!d_rem[index])                                                   //If current vertex is already colored, return
+        return;
+
+    int i, j, maxColours = maxDegree + 1;
+
+    for(i = d_adj_p[index]; i < d_adj_p[index + 1]; i++){               //Check if any uncolored neighbour has higher weight or if equal, higher index       
+        j = d_adj[i];                                                                   
+        if(d_rem[j] && ((d_weights[j] > d_weights[index]) || (d_weights[j] == d_weights[index] && j>index)) )  
+            return;
+    }
+
+    d_rem[index] = false;                                               //Current vertex should be coloured so remove it from remaining set
+
+    bool* forbidden = new bool[maxColours + 1];
+    for(i = 0; i < maxColours + 1; i++)
+        forbidden[i] = false;
+
+    for(i = d_adj_p[index]; i < d_adj_p[index + 1]; i++){               //Find out neighbours colors
+        j = d_adj[i];
+        forbidden[d_colours[j]] = true;
+    }
+    for(i = 1; i <= maxColours; i++){                                   //Assign color which is not there in neighbours' colors
+        if(!forbidden[i]){
+            d_colours[index] = i;
+            delete [] forbidden;
+            return;
+        }
+    }
+    delete [] forbidden;
+}
+
+int* colourGraph(int n, int m, int maxDegree, int* d_adj, int* d_adj_p, int* d_weights){
+    int i, rem_size = n, mx = min(n, 512);                                               //Initialize all variables
+    bool* h_rem = new bool[n];
+    bool* h_inc = new bool[n];
+    int* h_colours = new int[n];
+    bool* d_rem;
+    int *d_colours;
+
+    cudaMalloc((void**)&d_colours, sizeof(int) * n);                                        //Allocate space on GPU
+    cudaMalloc((void**)&d_rem, sizeof(bool) * n);
+
+    initializeRemCol<<<mx, mx>>>(d_rem, d_colours, n);                                       
+
+    while(rem_size > 0){
+        // cout << rem_size << endl;
+        colorSet<<<mx, mx>>>(d_adj, d_adj_p, d_weights, d_rem, d_colours, n, maxDegree);    //Launch kernel
+        cudaMemcpy(h_rem, d_rem, sizeof(bool) * n, cudaMemcpyDeviceToHost);                 //Copy back the updated uncolored set
+        int k = 0;
+        for(i = 0; i < n; i++){
+            if(h_rem[i])
+                k++;
+        }
+        rem_size = k;
+    }
+
+    cudaMemcpy(h_colours, d_colours, sizeof(int) * n, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_colours);                                                                    //Free memory
+    cudaFree(d_rem);
+
+    return h_colours;
+}
+
 int main()
 {
     int n=0, m=0, maxDegree=0, minDegree=0;
@@ -159,7 +238,7 @@ int main()
     
     cudaMemcpy(d_adj, h_adj, sizeof(int) * ((2 * m) + 1), cudaMemcpyHostToDevice);
     cudaMemcpy(d_adj_p, h_adj_p, sizeof(int) * (n + 1), cudaMemcpyHostToDevice);
-    
+
     int* h_weights = new int[n];    
     bool* h_visited = new bool[n];
     
@@ -171,7 +250,7 @@ int main()
     int max_wt=INT_MIN;
     for(int i=0;i<n;i++)
     {
-        cout << "Weight of " << i << "is " << h_weights[i] << endl;
+        cout << "Weight of " << i << " is " << h_weights[i] << endl;
         max_wt = max(max_wt, h_weights[i]);
     }
         
@@ -179,32 +258,32 @@ int main()
     
     int* h_colors = new int[n];
     memset(h_colors,0,sizeof(int)*n);
-    bool* h_sameWt = new bool[n];
-    memset(h_sameWt, false, sizeof(bool)*n);
 
     int* d_colors, *d_weights;
-    bool* d_sameWt;
     
     cudaMalloc((void**)&d_weights, sizeof(int) * n);
     cudaMalloc((void**)&d_colors, sizeof(int) * n);
-    cudaMalloc((void**)&d_sameWt, sizeof(bool) * n);
     
     cudaMemcpy(d_weights, h_weights, sizeof(int) * n, cudaMemcpyHostToDevice);
     cudaMemcpy(d_colors, h_colors, sizeof(int) * n, cudaMemcpyHostToDevice);
    
-    int mx = min(n,512);
-    for(int i = max_wt; i>=1; i--)
-    {
-        cudaMemcpy(d_sameWt, h_sameWt, sizeof(bool) * n, cudaMemcpyHostToDevice);
-        colorKernel<<<mx,mx>>>(n, i, maxDegree, d_adj, d_adj_p, d_weights, d_colors, d_sameWt);
-    }
-    cudaMemcpy(h_colors, d_colors, sizeof(int) * n, cudaMemcpyDeviceToHost);
+    // int* h_rweights = new int[n];
+    // int* d_rweights;
+    // cudaMalloc((void**)&d_rweights, sizeof(int) * n);
+    // randomWeightAssign(h_rweights, n);
+    // cudaMemcpy(d_rweights, h_rweights, sizeof(int) * n, cudaMemcpyHostToDevice);
+
     // for(int i=0;i<n;i++)
-    //     cout << "Color of " << i << "is" << h_colors[i] << endl;
+    //     cout << i << " " << h_rweights[i] << endl;
+
+    h_colors = colourGraph(n, m, maxDegree, d_adj, d_adj_p, d_weights);
+
+    for(int i=0;i<n;i++)
+        cout << "Color of vertex " << i << " is " << h_colors[i] << endl;
 
     cudaFree(d_adj);
     cudaFree(d_adj_p);
     cudaFree(d_colors);
-    cudaFree(d_sameWt);
     cudaFree(d_weights);
+    // cudaFree(d_rweights);
 }
